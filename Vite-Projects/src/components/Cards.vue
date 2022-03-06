8da263f33cbb224d96190efc1daa8b4d9074954c
Suran <suranbasharati@gmail.com>
2022-03-06 10:03:48 +0100

    Moved popup to Cards.vue

--- Vite-Projects/src/components/Cards.vue
+++ Vite-Projects/src/components/Cards.vue
@@ -1,19 +1,39 @@
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
 const companyName: String = "Ganske Enkelt AS"; //ref(new Date())
 const personName: String = "Suran Basharati"; //ref(new Date())
+let dialogTableVisible = ref(false)
+let dialogFormVisible = ref(false)
+const formLabelWidth = '140px'
+//const props = defineProps(['showDialog'])
 
 let items = [
-  { "name": "SuperOffice Norge AS", "src": "./src/assets/company.svg" },
-  { "name": "Suran Basharati", "src": "./src/assets/person.svg" },
-  { "name": "Opprett Sak", "src": "./src/assets/request.svg" },
-  { "name": "Opprett Aktivitet", "src": "./src/assets/calender.svg" },
-  { "name": "Vis firma", "src": "./src/assets/company.svg" }
+  { "id": 1, "name": "SuperOffice Norge AS", "src": "./src/assets/company.svg" },
+  { "id": 2, "name": "Suran Basharati", "src": "./src/assets/person.svg" },
+  { "id": 3, "name": "Opprett Sak", "src": "./src/assets/request.svg" },
+  { "id": 4, "name": "Opprett Aktivitet", "src": "./src/assets/calender.svg" },
+  { "id": 5, "name": "Vis firma", "src": "./src/assets/company.svg" }
 ];
 
-const cardClick = (element: String) => {
-  console.log(element);
+let defaultTime = new Date()
+console.log(defaultTime);
+
+const cardClick = (id: number) => {
+  if(id == 4)
+  dialogFormVisible.value = true;
 }
+
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
 
 </script>
 <template>
@@ -22,9 +42,8 @@ const cardClick = (element: String) => {
       <el-card 
       :body-style="{ padding: '0px' }"
       v-for="item in items"
-     :key="item.name"
-      >
-      <div class="inner" @click="cardClick(item.name)">
+     :key="item.name">
+      <div class="inner" @click="cardClick(item.id)">
         <img
         :src="item.src"
           class="image">
@@ -35,6 +54,66 @@ const cardClick = (element: String) => {
       </el-card>
     </el-col>
   </el-row>
+  
+  <el-dialog v-model="dialogFormVisible" title="Opprett Aktivitet">
+    <el-form :model="form">
+      <!-- Aktivitets Type -->
+      <el-form-item label="Type Aktivitet" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="Velg aktivitet">
+          <el-option label="Zone No.1" value="shanghai"></el-option>
+          <el-option label="Zone No.2" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <!-- Firma -->
+      <el-form-item label="Firma" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <!-- Person -->
+      <el-form-item label="Person" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+
+      <!-- Start -->
+      <div class="demo-datetime-picker">
+        <el-form-item label="Start" :label-width="formLabelWidth">
+<div class="block">
+          <el-date-picker
+            v-model="form.date1"
+            type="datetime"
+            placeholder="Select date and time"
+            :default-time="defaultTime"
+            >
+          </el-date-picker>
+          </div>
+        </el-form-item>
+        <!-- Slutt -->
+        <el-form-item label="Slutt" :label-width="formLabelWidth">
+          <div class="block">
+          <el-date-picker
+            v-model="form.date2"
+            type="datetime"
+            placeholder="Select date and time"
+            :default-time="defaultTime"
+            >
+          </el-date-picker>
+          </div>
+        </el-form-item>
+      </div>
+      <!-- Beskrivelse -->
+      <el-form-item label="Beskrivelse" :label-width="formLabelWidth">
+        <el-input v-model="form.desc" type="textarea" :row="8" autocomplete="off"></el-input>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >Confirm</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+
 </template>
 
 <style>
@@ -62,5 +141,40 @@ const cardClick = (element: String) => {
 .image {
   padding-top: 5%;
   width: 10%;
+}
+
+.el-button--text {
+  margin-right: 15px;
+}
+.el-select {
+  width: 300px;
+}
+.el-input {
+  width: 300px;
+}
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+
+.demo-datetime-picker {
+  display: flex;
+  width: 100%;
+  padding: 0;
+  flex-wrap: wrap;
+}
+.demo-datetime-picker .block {
+  padding: 30px 0;
+  text-align: center;
+  border-right: solid 1px var(--el-border-color-base);
+  flex: 1;
+}
+.demo-datetime-picker .block:last-child {
+  border-right: none;
+}
+.demo-datetime-picker .demonstration {
+  display: block;
+  color: var(--el-text-color-secondary);
+  font-size: 14px;
+  margin-bottom: 20px;
 }
 </style>
---
+++ Vite-Projects/src/components/CreateActivity.vue
@@ -0,0 +1,80 @@
+<template>
+  <el-dialog v-model="dialogFormVisible" title="Opprett Aktivitet">
+    <el-form :model="form">
+      <el-form-item label="Promotion name" :label-width="formLabelWidth">
+        <el-input v-model="form.name" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="Zones" :label-width="formLabelWidth">
+        <el-select v-model="form.region" placeholder="Please select a zone">
+          <el-option label="Zone No.1" value="shanghai"></el-option>
+          <el-option label="Zone No.2" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false"
+          >Confirm</el-button
+        >
+      </span>
+    </template>
+  </el-dialog>
+</template>
+<script lang="ts" setup>
+import { reactive, ref } from 'vue'
+const dialogTableVisible = ref(false)
+const dialogFormVisible = ref(false)
+const formLabelWidth = '140px'
+
+const props = defineProps(['activity'])
+
+
+const form = reactive({
+  name: '',
+  region: '',
+  date1: '',
+  date2: '',
+  delivery: false,
+  type: [],
+  resource: '',
+  desc: '',
+})
+
+const gridData = [ 
+  {
+    date: '2016-05-02',
+    name: 'John Smith',
+    address: 'No.1518,  Jinshajiang Road, Putuo District',
+  },
+  {
+    date: '2016-05-04',
+    name: 'John Smith',
+    address: 'No.1518,  Jinshajiang Road, Putuo District',
+  },
+  {
+    date: '2016-05-01',
+    name: 'John Smith',
+    address: 'No.1518,  Jinshajiang Road, Putuo District',
+  },
+  {
+    date: '2016-05-03',
+    name: 'John Smith',
+    address: 'No.1518,  Jinshajiang Road, Putuo District',
+  },
+]
+</script>
+<style scoped>
+.el-button--text {
+  margin-right: 15px;
+}
+.el-select {
+  width: 300px;
+}
+.el-input {
+  width: 300px;
+}
+.dialog-footer button:first-child {
+  margin-right: 10px;
+}
+</style>
